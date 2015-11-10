b1ce7055513882a13a6edc8ab19cd788701ce7db
Sebastian Ortiz V <neoecos@gmail.com>
2015-11-09 23:35:32 -0500

    Get tpaga api key from env

--- tpaga_config.rb
+++ tpaga_config.rb
@@ -5,5 +5,5 @@ Tpaga::Swagger.configure do |config|
   config.host = 'sandbox.tpaga.co'
   config.base_path = '/api'
   config.inject_format = false
-  config.api_key = 'd13fr8n7vhvkuch3lq2ds5qhjnd2pdd2'
+  config.api_key = ENV["TPAGA_API_KEY"]
 end
