592ed601a2043f13ccb2e0966c484751ac3a16b3
Sebastian Ortiz V <neoecos@gmail.com>
2015-11-10 00:34:01 -0500

    Adds support for adding cards and create charges

--- tpaga_demo.rb
+++ tpaga_demo.rb
@@ -23,12 +23,56 @@ module TpagaDemo
         requires :phone, type: String
       end
 
-      customer = Tpaga::Customer.new(firstName: params[:firstName], lastName: params[:lastName],
+      customer_local = Tpaga::Customer.new(firstName: params[:firstName], lastName: params[:lastName],
       email: params[:email], phone: params[:phone])
       customer = Tpaga::CustomerApi.create_customer(customer)
       p customer
       customer.to_hash
     end
 
+    post '/card' do
+      params do
+        requires :cardNumber, type: String
+        requires :expMonth, type: String
+        requires :expYear, type: String
+        requires :cvc, type: String
+        requires :cardName, type: String
+        requires :userId, type: String
+      end
+      card_local = Tpaga::CreditCardCreate.new(
+          primaryAccountNumber: params[:cardNumber],
+          expirationMonth: params[:expMonth],
+          expirationYear: params[:expYear],
+          cardVerificationCode: params[:cvc],
+          cardHolderName: params[:cardName],
+          billingAddress: Tpaga::BillingAddress.new
+      )
+      card = Tpaga::CreditCardApi.add_credit_card(params[:userId], card_local)
+      p card
+      card.to_hash
+    end
+
+    post '/charge' do
+      params do
+        requires :amount, type: Integer
+        requires :iva, type: Integer
+        requires :description, type: String
+        requires :orderNumber, type: String
+        requires :installments, type: Integer
+        requires :cardId, type: String
+      end
+      charge_local = Tpaga::CreditCardCharge.new(
+        amount: params[:amount],
+        taxAmount: params[:iva],
+        currency: "COP",
+        orderId: params[:orderNumber],
+        installments: params[:installments],
+        description: params[:description],
+        creditCard: params[:cardId]
+      )
+      charge = Tpaga::ChargeApi.add_credit_card_charge(charge_local)
+      p charge
+      charge.to_hash
+    end
   end
 end
