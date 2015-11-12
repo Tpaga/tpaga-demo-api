b7210951a8666b0611d583cbf274ddcd2b5889e5
Sebastian Ortiz V <neoecos@gmail.com>
2015-11-11 20:53:43 -0500

    changed to prod

--- tpaga_config.rb
+++ tpaga_config.rb
@@ -2,7 +2,7 @@ require 'tpaga'
 
 Tpaga::Swagger.configure do |config|
   config.scheme = 'https'
-  config.host = 'sandbox.tpaga.co'
+  config.host = 'api.tpaga.co'
   config.base_path = '/api'
   config.inject_format = false
   config.api_key = ENV["TPAGA_API_KEY"]
